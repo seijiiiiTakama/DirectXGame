35106b54866745952540df01247ebdee1028e3b1
seijiiiiTakama <115197505+seijiiiiTakama@users.noreply.github.com>
2023-04-23 02:23:05 +0900

    Update main.cpp
    タイトルバーを変更

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"GC2A_13_タカマツ_セイジ_AL3");
+	win->CreateGameWindow(L"GC2A_13_タカマツ_セイジ_AL3　");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
